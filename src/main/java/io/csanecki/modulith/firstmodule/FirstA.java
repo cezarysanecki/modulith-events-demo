32a697a745ce4bfb23c1b508c36618762a845e2e
Cezary Sanecki <devcezz@gmail.com>
2023-02-10 09:41:48 +0100

    Changed annotations

--- src/main/java/io/csanecki/modulith/firstmodule/FirstA.java
+++ src/main/java/io/csanecki/modulith/firstmodule/FirstA.java
@@ -2,8 +2,10 @@ package io.csanecki.modulith.firstmodule;
 
 import io.csanecki.modulith.firstmodule.internal.FirstInternalA;
 import io.csanecki.modulith.secondmodule.SecondModuleEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class FirstA {
@@ -14,7 +16,9 @@ public class FirstA {
         this.firstInternalA = firstInternalA;
     }
 
-    @EventListener
+    @Async
+    @Transactional
+    @TransactionalEventListener
     public void handle(SecondModuleEvent event) {
         System.out.println("i've got message!");
     }
