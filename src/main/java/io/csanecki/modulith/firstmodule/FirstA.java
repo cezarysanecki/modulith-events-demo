fe6d4792c8538a249bc36134164ce3d9517af617
Cezary Sanecki <devcezz@gmail.com>
2023-02-10 08:45:18 +0100

    Handled event

--- src/main/java/io/csanecki/modulith/firstmodule/FirstA.java
+++ src/main/java/io/csanecki/modulith/firstmodule/FirstA.java
@@ -1,14 +1,21 @@
 package io.csanecki.modulith.firstmodule;
 
-import org.springframework.stereotype.Component;
 import io.csanecki.modulith.firstmodule.internal.FirstInternalA;
+import io.csanecki.modulith.secondmodule.SecondModuleEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @Component
 public class FirstA {
 
-  private final FirstInternalA firstInternalA;
+    private final FirstInternalA firstInternalA;
 
-  FirstA(FirstInternalA firstInternalA) {
-    this.firstInternalA = firstInternalA;
-  }
+    FirstA(FirstInternalA firstInternalA) {
+        this.firstInternalA = firstInternalA;
+    }
+
+    @EventListener
+    public void handle(SecondModuleEvent event) {
+        System.out.println("i got message!");
+    }
 }
