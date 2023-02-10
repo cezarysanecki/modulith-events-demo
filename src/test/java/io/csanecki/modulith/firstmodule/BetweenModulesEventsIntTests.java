c27f981693d30aedcc9d64b9b67643ea21276f53
Cezary Sanecki <devcezz@gmail.com>
2023-02-10 12:48:13 +0100

    Added test

---
+++ src/test/java/io/csanecki/modulith/firstmodule/BetweenModulesEventsIntTests.java
@@ -0,0 +1,29 @@
+package io.csanecki.modulith.firstmodule;
+
+import io.csanecki.modulith.secondmodule.SecondA;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.modulith.events.EventPublicationRepository;
+import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.ApplicationModuleTest.BootstrapMode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationModuleTest(value = BootstrapMode.ALL_DEPENDENCIES)
+class BetweenModulesEventsIntTests {
+
+    @Autowired
+    private SecondA secondA;
+
+    @Autowired
+    private EventPublicationRepository eventPublicationRepository;
+
+    @Test
+    void emit_event_exactly_once() {
+        secondA.doSomething();
+
+        var result = eventPublicationRepository.findIncompletePublications();
+        assertThat(result).hasSize(1);
+    }
+
+}
